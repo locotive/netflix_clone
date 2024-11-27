01e18d4f2325c256f663b0e6b4523c73ca07a353
locotive <locotive@naver.com>
2024-11-27 19:25:05 +0900

    fix: update router history base path for deployment

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/netflix_clone/'),
   routes: [
     {
       path: '/signin',
